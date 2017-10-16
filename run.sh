ed53a5e946b556b76c05bbfe356dd7f1862c50e7
qHe-intek <qidu.he@intel.com>
2017-10-16 14:10:34 +0800

    pass daily to python script

--- run.sh
+++ run.sh
@@ -7,20 +7,20 @@ export KMP_AFFINITY=compact,1,0,granularity=fine
 
 if [ $t == 'bdw' ]; then
   export OMP_NUM_THREADS=44
-  python LSTM.py $2
+  python LSTM.py $2 $3
 fi
 if [ $t == 'knl' ]; then
   export OMP_NUM_THREADS=68
-  python LSTM.py $2
+  python LSTM.py $2 $3
 fi
 if [ $t == 'knm' ]; then
   export OMP_NUM_THREADS=72
-  python LSTM.py $2
+  python LSTM.py $2 $3
 fi
 if [ $t == 'skx' ]; then
   export OMP_NUM_THREADS=56
-  python LSTM.py $2
+  python LSTM.py $2 $3
 fi
 if [ $t == 'gpu' ]; then
-  python LSTM.py $2 cuda
+  python LSTM.py $2 cuda $3
 fi
