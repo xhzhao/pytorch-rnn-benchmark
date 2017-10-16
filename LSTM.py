4b5d4574b42e3e1c846a10ab02ed6ea908267b81
qHe-intek <qidu.he@intel.com>
2017-10-16 10:26:23 +0800

    add daily option

--- LSTM.py
+++ LSTM.py
@@ -8,12 +8,20 @@ import sys
 count = 20     # number of iterations
 cuda = False   # whether GPU is used or not
 train = False  # True: test training performance; False: test forward performance only
+daily = False
 if 'cuda' in sys.argv:
     cuda = True
 if 'train' in sys.argv:
     train = True
+if 'daily' in sys.argv:
+    daily = True
 
-sizes = [[64,15,500,500],
+if daily:
+    sizes = [[64,50,500,500],
+         [128,25,4096,4096]
+        ]
+else:
+    sizes = [[64,15,500,500],
          [64,20,500,500],
          [64,25,500,500],
          [64,30,500,500],
@@ -36,47 +44,46 @@ sizes = [[64,15,500,500],
          [16,25,4096,4096],
          [32,25,4096,4096],
          [64,25,4096,4096],
-         [128,25,4096,4096],
-         [64,50,500,500]
+         [128,25,4096,4096]
         ]
 
 
 for idx in range(len(sizes)):
-  size = sizes[idx]
-  N = size[0]    # batch size
-  T = size[1]    # sentence length
-  D = size[2]    # embedding size
-  H = size[3]    # hidden size
+    size = sizes[idx]
+    N = size[0]    # batch size
+    T = size[1]    # sentence length
+    D = size[2]    # embedding size
+    H = size[3]    # hidden size
   
-  if cuda:
-    rnn = nn.LSTM(D,H,1).cuda()
-    input = Variable(torch.randn(N, T, D).cuda())
-    h0 = Variable(torch.randn(1, T, H).cuda())
-    c0 = Variable(torch.randn(1, T, H).cuda())
-  else:
-    rnn = nn.LSTM(D,H,1)
-    input = Variable(torch.randn(N, T, D))
-    h0 = Variable(torch.randn(1, T, H))
-    c0 = Variable(torch.randn(1, T, H))
+    if cuda:
+        rnn = nn.LSTM(D,H,1).cuda()
+        input = Variable(torch.randn(N, T, D).cuda())
+        h0 = Variable(torch.randn(1, T, H).cuda())
+        c0 = Variable(torch.randn(1, T, H).cuda())
+    else:
+        rnn = nn.LSTM(D,H,1)
+        input = Variable(torch.randn(N, T, D))
+        h0 = Variable(torch.randn(1, T, H))
+        c0 = Variable(torch.randn(1, T, H))
 
-  output, hn = rnn(input, (h0, c0))
-  if train:
-    loss_fn = nn.MSELoss().cuda()
-
-  start = time.time()
-  for j in range(count):
     output, hn = rnn(input, (h0, c0))
     if train:
-      if cuda:
-        targets = Variable(torch.randn(N,T,D).cuda())
-      else:
-        targets = Variable(torch.randn(N,T,D))
-      loss = loss_fn(output,targets)
-      loss.backward()
-    if cuda:
-      torch.cuda.synchronize()
-  dura = (time.time() - start)/count     # time of ONE iteration
-  gflops = T*4*(N*H*D*2 + N*H*H*2)/1e9
-  GFLOPS = gflops/dura                   # giga floating-point operations per second
-  SPS = N/dura                           # number of processed sentences per second
-  print("size = %s, duration = %.4f, gflops = %.4f, GFLOPS = %.4f, SPS = %.4f" %(size,dura,gflops,GFLOPS,SPS))
+        loss_fn = nn.MSELoss().cuda()
+
+    start = time.time()
+    for j in range(count):
+        output, hn = rnn(input, (h0, c0))
+        if train:
+            if cuda:
+                targets = Variable(torch.randn(N,T,D).cuda())
+            else:
+                targets = Variable(torch.randn(N,T,D))
+            loss = loss_fn(output,targets)
+            loss.backward()
+        if cuda:
+            torch.cuda.synchronize()
+    dura = (time.time() - start)/count     # time of ONE iteration
+    gflops = T*4*(N*H*D*2 + N*H*H*2)/1e9
+    GFLOPS = gflops/dura                   # giga floating-point operations per second
+    SPS = N/dura                           # number of processed sentences per second
+    print("size = %s, duration = %.4f, gflops = %.4f, GFLOPS = %.4f, SPS = %.4f" %(size,dura,gflops,GFLOPS,SPS))
--- README.md
+++ README.md
@@ -8,8 +8,9 @@ A program to test performance of several RNNs(realized by Pytorch) on different 
 
 ### Usage
 ****
-	bash run.sh platform-name [train]
+	bash run.sh platform-name [train] [daily]
 platform-name can be 'bdw', 'skx', 'knm', 'knl', 'gpu'.   
-If you want to test performance of training, add parameter train.  
+If you want to test performance of training, please add parameter train.  
+If you want to run daily test(just test 2 size), please add parameter daily.  
 
 
